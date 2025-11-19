0383174b2e8486b3fb31170b1b108d4e4bb03bf8
Steve Blum <stevebblum@gmail.com>
2025-11-19 20:20:21 +0000

    ADDED INCREMENTAL

---
+++ models/demo/sales.sql
@@ -0,0 +1,27 @@
+{{
+    config
+    (
+        materialized='incremental',
+        incremental_stategy = 'append'
+    )
+}}
+
+with sales_src as 
+(
+        select
+        SALE_ID,
+        SALE_DATE,
+        CUSTOMER_ID,
+        PRODUCT_ID,
+        QUANTITY,
+        TOTAL_AMOUNT,
+        CREATED_AT,
+        CURRENT_TIMESTAMP AS INSERT_DTS
+        FROM {{source('sales','SALES_SRC')}}
+
+        {% if is_incremental() %}
+        WHERE CREATED_AT > (SELECT MAX(INSERT_DTS) FROM {{this}})
+        {% endif %}
+)
+
+SELECT * FROM sales_src
